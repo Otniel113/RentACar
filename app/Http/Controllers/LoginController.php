c4f4ea33b0d6f8ca9586a5bc250055fadfa49b63
gilangrizky <gilangcb@student.telkomuniversity.ac.id>
2022-04-12 18:22:32 +0700

    Repair logout & View Mobil Kecil

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -38,8 +38,8 @@ class LoginController extends Controller
     public function logout(Request $request)
     {
         Auth::logout();
-        $request->session->invalidate();
-        $request->session->regenerateToken();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
 
         return redirect('/');
     }
--- resources/views/product/mobilkecil.blade.php
+++ resources/views/product/mobilkecil.blade.php
@@ -1,55 +1,70 @@
 <html>
-    <head>
-        <link href="{{ ('/css/product/bus.css') }}" rel="stylesheet">
-        <script src="{{ ('/js/product/main.js') }}"></script>
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-        <script src="{{ asset('assets/jquery.js') }}"></script>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF" crossorigin="anonymous"></script>
-    </head>
-    <body>
-        <!-- Nav -->
-        <div class="navigation">
-            <nav>
-                <ul class="nav justify-content-center">
+
+<head>
+    <link href="{{ '/css/product/bus.css' }}" rel="stylesheet">
+    <script src="{{ '/js/product/main.js' }}"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css"
+        integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <script src="{{ asset('assets/jquery.js') }}"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF" crossorigin="anonymous">
+    </script>
+</head>
+
+<body>
+    <!-- Nav -->
+    <div class="navigation">
+        <nav>
+            <ul class="nav justify-content-center">
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ '/home' }}">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ '/product' }}">Product</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ '/profile' }}">Profile</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ '/gallery' }}">Gallery</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ '/faq' }}">FAQ</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ '/contactus' }}">Contact Us</a>
+                </li>
+                @auth
                     <li class="nav-item">
-                        <a class="nav-link" href="{{ ('/home') }}" >Home</a>
+                        <a class="nav-link" href="#">Welcome Back, {{ auth()->user()->name }}</a>
                     </li>
+                    <form action="/logout" method="post">
+                        @csrf
+                        <button type="submit" class="btn btn-danger">Logout</button>
+                    </form>
+                @else
                     <li class="nav-item">
-                        <a class="nav-link" href="{{ ('/product') }}">Product</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ ('/profile') }}">Profile</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ ('/gallery') }}">Gallery</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ ('/faq') }}">FAQ</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ ('/contactus') }}">Contact Us</a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="{{ ('/login') }}">
+                        <a href="login">
                             <button type="button" class="btn btn-primary">Login</button>
                         </a>
                     </li>
                     <li class="nav-item">
-                        <a href="{{ ('/register') }}">
+                        <a href="register">
                             <button type="button" class="btn btn-success">Register</button>
                         </a>
                     </li>
-                </ul>	
-            </nav>
-        </div>
-        <!-- End Nav -->
+                @endauth
+            </ul>
+        </nav>
+    </div>
+    <!-- End Nav -->
 
-        <!-- Body -->
-        <!-- Content 1 -->
-        <div class="container">
+    <!-- Body -->
+    <!-- Content 1 -->
+    <div class="container">
 
         <!-- <div class="card-deck">
-            <a href="{{ ('/product/busdetails1') }}">
+            <a href="{{ '/product/busdetails1' }}">
                 <div class="card bg-dark">
                     <div class="card-body text-left">
                         <div>
@@ -59,7 +74,7 @@
                         <p class="card-text" style="color: white;">Capacity : 52 Seats</p>
                         <p class="card-text" style="color: white;">Fuel     : Solar</p>
                     </div>
-                    <a href="{{ ('/product/busdetails1') }}" align="center">
+                    <a href="{{ '/product/busdetails1' }}" align="center">
                         <button type="button" class="btn btn-info">View More Details</button>
                     </a><br>
                 </div>
@@ -75,13 +90,13 @@
                         <p class="card-text" style="color: white;">Capacity : 32 Seats</p>
                         <p class="card-text" style="color: white;">Fuel     : Solar</p>
                     </div>
-                    <a href="{{ ('/product') }}" align="center">
+                    <a href="{{ '/product' }}" align="center">
                         <button type="button" class="btn btn-info">View More Details</button>
                     </a><br>
                 </div>
             </a> -->
-            <h2 style="text-align: center;">Mobil Kecil</h2>
-            <div class="row justify-content-center" style="margin-top:5%">
+        <h2 style="text-align: center;">Mobil Kecil</h2>
+        <div class="row justify-content-center" style="margin-top:5%">
             <div class="col-10">
                 <span class="float-left">{{ session('msg') }}</span>
                 <a href="/product/mobilkecil/create" class="btn btn-secondary float-right">Tambah</a><br /><br />
@@ -94,30 +109,31 @@
                         <th>Ketersediaan</th>
                         <th>Aksi</th>
                     </tr>
-                    @foreach($list as $d)
-                    <tr>
-                        <td>{{ $d->nama_mobil }}</td>
-                        <td>{{ $d->no_polisi }}</td>
-                        <td>{{ $d->driver }}</td>
-                        <td>{{ $d->wisata }}</td>
-                        <td>{{ $d->ketersediaan }}</td>
-                        <td>
-                        <a href="/product/mobilkecil/{{ $d->id }}/edit" class="btn btn-primary">Edit</a>
-                        <form method="post" action="/product/mobilkecil/{{ $d->id }}"
-                            style="display:inline" onsubmit="return confirm('Yakin hapus?')">
-                            @csrf
-                            @method('DELETE')
-                            <button class="btn btn-danger">Hapus</button>
-                        </form>
-                        </td>
-                    </tr>
+                    @foreach ($list as $d)
+                        <tr>
+                            <td>{{ $d->nama_mobil }}</td>
+                            <td>{{ $d->no_polisi }}</td>
+                            <td>{{ $d->driver }}</td>
+                            <td>{{ $d->wisata }}</td>
+                            <td>{{ $d->ketersediaan }}</td>
+                            <td>
+                                <a href="/product/mobilkecil/{{ $d->id }}/edit" class="btn btn-primary">Edit</a>
+                                <form method="post" action="/product/mobilkecil/{{ $d->id }}"
+                                    style="display:inline" onsubmit="return confirm('Yakin hapus?')">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button class="btn btn-danger">Hapus</button>
+                                </form>
+                            </td>
+                        </tr>
                     @endforeach
                 </table>
             </div>
         </div>
 
-        </div>
-            <!-- End Content 1 -->
-        <!-- End Body -->
-    </body>
+    </div>
+    <!-- End Content 1 -->
+    <!-- End Body -->
+</body>
+
 </html>
