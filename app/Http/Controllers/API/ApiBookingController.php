82dc6ce03eecfc64f429c105791305408347fa00
Otniel113 <otniel.abiezer.saragih@gmail.com>
2022-06-27 09:30:10 +0700

    Memperbaiki error

--- app/Http/Controllers/API/ApiBookingController.php
+++ app/Http/Controllers/API/ApiBookingController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 use App\Models\Booking;
 use App\Http\Controllers\Controller as Controller;
 
-class BookingController extends Controller
+class ApiBookingController extends Controller
 {
     /**
      * Display a listing of the resource.
--- database/seeders/BookingsTableSeeder.php
+++ database/seeders/BookingsTableSeeder.php
@@ -14,21 +14,21 @@ class BookingsTableSeeder extends Seeder
     public function run()
     {
         \App\Models\Booking::create([
-            'id_mobil' => 1,
+            'id_mobil' => "1",
             'nama_mobil' => 'Toyota Avanza',
             'jenis_mobil' => 'Mobil Kecil',
             'wisata' => 'Tangkuban Perahu',
-            'id_member' => 1,
+            'id_member' => "1",
             'nama_member' => 'Otniel Abiezer',
             'status' => 'Menunggu Persetujuan'
         ]);
 
         \App\Models\Booking::create([
-            'id_mobil' => 2,
+            'id_mobil' => "2",
             'nama_mobil' => 'Honda Jazz',
             'jenis_mobil' => 'Mobil Kecil',
             'wisata' => 'Ciwidey',
-            'id_member' => 2,
+            'id_member' => "2",
             'nama_member' => 'Winico Fazry',
             'status' => 'Menunggu Persetujuan'
         ]);
